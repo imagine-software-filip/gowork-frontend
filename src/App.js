ea118404b1314e1ee83f568fa97bbd9c71f568c7
dimitr36 <dimitr36@msu.edu>
2023-02-12 17:45:46 -0500

    test netlify

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>Welcome</h1>
+        <p>Delete this rendering</p>
       </header>
     </div>
   );
