7d44de3a17362cf15c3782142d95609dc6a95509
dimitr36 <dimitr36@msu.edu>
2023-03-04 15:49:13 -0500

    Profile page demo

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,12 +1,36 @@
 import React from "react";
-import styles from './ProfileScreen.module.css';
+import { Link } from "react-router-dom";
+import styles from "./ProfileScreen.module.css";
 
 const ProfileScreen = () => {
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.titleContainer}>
-        <h3 style={{marginLeft: 20}}>Profile Screen</h3>
+      <div style={{backgroundColor: "#efefef", width: "90%", textAlign: "center", borderRadius: 10, paddingTop: 20}}>
+        <img
+          src="https://images.pexels.com/photos/2748242/pexels-photo-2748242.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt="Michael Scott"
+          className={styles.userImg}
+        />
+        <h2 className={styles.userName}>Linda Decer</h2>
       </div>
+      <div className={styles.settingsContainer}>
+        <Link to="/" className={styles.setting}>
+           Preferences
+        </Link>
+        <div style={{ height: 1, backgroundColor: "#c8c8c8" }}></div>
+        <Link to="/" className={styles.setting}>
+           Services
+        </Link>
+        <div style={{ height: 1, backgroundColor: "#c8c8c8" }}></div>
+        <Link to="/" className={styles.setting}>
+           Profile
+        </Link>
+        <div style={{ height: 1, backgroundColor: "#c8c8c8" }}></div>
+        <Link to="/" className={styles.setting}>
+          About
+        </Link>
+      </div>
+      <button className={styles.logoutBut}>Log Out</button>
     </div>
   );
 };
