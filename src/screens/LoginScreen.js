27368aa47ee7dee643cf0625ec145746007ffad6
Filip Dimitrievski <filipdimitrievski@protonmail.com>
2023-03-07 13:41:58 -0500

    Demo SignIn page

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,12 +1,41 @@
-import React from "react";
-import { Button, Form, Input } from "antd";
+import React, { useState } from "react";
+import { Button, Form, Input, Alert } from "antd";
 import { Link } from "react-router-dom";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import styles from "./LoginScreen.module.css";
 
 const LoginScreen = () => {
+  const [activeAlert, setActiveAlert] = useState(false);  
+  const [alert, setAlert] = useState({type: "", desc: "", msg: ""});
+
+  const AlertCard = () => {
+    return (
+      <Alert
+        message={alert.msg}
+        description={alert.desc}
+        type={alert.type}
+        showIcon
+        closable
+      />
+    );
+  };
+
   const onFinish = (values) => {
-    console.log("Received values of form: ", values);
+    localStorage.setItem("authToken", "");
+    console.log()
+    if (values) {
+      console.log("Received values of form: ", values);
+      localStorage.setItem("authToken", "asdas");
+    }
+
+    if (localStorage.getItem("authToken") !== "") {
+        setAlert({
+            type: 'error',
+            desc: 'User with the provided info does not exists',
+            msg: 'Failed'
+        });
+        setActiveAlert(true);
+    }
   };
 
   return (
@@ -65,8 +94,12 @@ const LoginScreen = () => {
               >
                 Log in
               </Button>
-              Or <Link to="/" className={styles.registerNowButt}>register now!</Link>
+              Or{" "}
+              <Link to="/" className={styles.registerNowButt}>
+                register now!
+              </Link>
             </Form.Item>
+            {activeAlert && <AlertCard msg="Congrats!" type="error" desc="Lorem ipsun lorem ipuss asd  a" />}
           </Form>
         </div>
       </div>
