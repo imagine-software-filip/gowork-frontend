e7d9902d46b15776d707b365ea2b65895d94ca20
dimitr36 <dimitr36@msu.edu>
2023-03-10 11:42:28 -0500

    Build netlify fix

--- src/components/System/VerifyUser.js
+++ src/components/System/VerifyUser.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { Input } from "antd";
-import { VERIFY_USER } from '../../GraphQL/mutations'
+import { VERIFY_USER } from "./../../GraphQL/Mutations"
 
 import styles from "./VerifyUser.module.css";
 
